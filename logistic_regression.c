71d637ae6188d93947d6c544c85c8a740eaae71e
wjlee <u201811417@hust.edu.cn>
2019-12-11 22:14:34 -1200

    v3.0

--- logistic_regression.c
+++ logistic_regression.c
@@ -13,9 +13,9 @@ int main(){
     FILE *fp;
     char strline[50]; //读取文件每行的buffer 
     int j = 0,i = 0,k = 0,t = 0;
-    float parameter[200][4];
+    float parameter[6][4];
     double theta[4]={2,2,2,2};
-    char result[200];
+    int result[6]={0};
     double h,error,derivate,test = 0;
     if((fp=fopen("D:\\c_tests\\data2.txt","r"))==NULL) //文件位置和文件名 
     {
@@ -25,33 +25,36 @@ int main(){
     while(!feof(fp))  //判断文件是否已到末尾 
     {
         fgets(strline,50,fp);  //读取一行 
-        sscanf(strline,"%f %f %f %f %c",&parameter[i][0],&parameter[i][1],&parameter[i][2],&parameter[i][3],&result[i]); 
-        i++;
+        sscanf(strline,"%f %f %f %f %u",&parameter[t][0],&parameter[t][1],&parameter[t][2],&parameter[t][3],&result[t]); 
+        t++;
     }
     fclose(fp);
     while(1)
     {
-        printf("\n%.3f %.3f %.3f %.3f %c\n",parameter[j][0],parameter[j][1],parameter[j][2],parameter[j][3],result[j]);
+        printf("\n%.3f %.3f %.3f %.3f %u\n",parameter[j][0],parameter[j][1],parameter[j][2],parameter[j][3],result[j]);
         j++;
-        if(j>=i)
+        if(j>=t)
         {
             break;
         }
     }
-    test = sigmoidFunction(1888);
-    for(k = 0;k < 1000;k++){
-        for(j = 0;j < i;j++){
-            h = 0;
-            for(t = 0;t < 4;t++){
-                h += parameter[j][t]*theta[t];
+    for(i=1;i<10000;i++)
+    {
+        for(j=0;j<6;j++)
+        {
+            h=0;
+            for(k=0;k<4;k++)
+            {
+                h += parameter[j][k]*theta[k];
             }
             error = sigmoidFunction(h) - result[j];
-            for(t = 0;t <4;t++){
-                derivate = (-1)*error*parameter[j][t];
-                theta[t] += derivate*0.05;
+            for(k=0;k<4;k++)
+            {
+                derivate = (-1)*error*parameter[j][k];
+                theta[k] += 0.01*derivate;
             }
-        }
-        printf("%f %f %f %f\n",theta[0],theta[1],theta[2],theta[3]);
+            printf("%lf %lf %lf %lf\n",theta[0],theta[1],theta[2],theta[3]); 
+        } 
     }
     getchar();
     getchar();
